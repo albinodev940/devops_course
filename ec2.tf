7f6a3fff497ab9e39856c22e83ca2969013b12a3
albinodev940 <albino.torales+dev@zoi.tech>
2025-09-02 14:49:51 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami = "ami-00aa4671cbf840d82"
   instance_type = "t2.micro"
   tags = {
-    Name = "devops-workshop-zoi"
+    Name = "devops-workshop-zoi-albino"
   }
 }
